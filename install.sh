45c52014d6c22866587774e398e6fb758d2e3cb7
roman.kozin <roman.kozin@cyberchisel.com>
2016-06-06 16:54:01 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing a2hoster globally"
-su
-mv a2hoster.sh /usr/local/bin/a2hoster
-mv a2hoster.func.sh /usr/local/bin/
+sudo mv a2hoster.sh /usr/local/bin/a2hoster >> /dev/null
+sudo mv a2hoster.func.sh /usr/local/bin/  >> /dev/null
+sudo chmod 744 /usr/local/bin/a2hoster  >> /dev/null
 echo "Installing complete. Check functionality by running shell with sudo a2hoster"
