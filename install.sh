a94f0ecb7f40ce04efd99c34b8e2a4f7241c7e4a
roman.kozin <romanko95.rk@gmail.com>
2018-02-08 23:45:19 +0200

    Updated Installer

--- install.sh
+++ install.sh
@@ -4,22 +4,13 @@ sudo cp a2hoster.sh /usr/local/bin/a2hoster >> /dev/null
 sudo cp a2hoster.func.sh /usr/local/bin/  >> /dev/null
 
 # Installing server part if not exists
-sudo apt-get install apache2
-sudo apt-get install php
-sudo apt-get install php5-intl || sudo apt-get install php-intl
-sudo apt-get install php5-mcrypt || sudo apt-get install php-mcrypt
-sudo apt-get install php5-mysql || sudo apt-get install php-mysql
-sudo apt-get install mysql-server-* mysql-client-*
-sudo apt-get install libapache2-mod-php || libapache2-mod-php7.0 || libapache2-mod-php5
+sudo apt-get install apache2 composer git php php5-intl php-intl php5-mcrypt php-mcrypt php5-mysql php-mysql mysql-server-* mysql-client-* libapache2-mod-php libapache2-mod-php7.0 libapache2-mod-php5 --force-yes
 sudo a2enmod rewrite
 
 sudo service apache2 restart
 
 # Installing composer globally
-sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo php composer-setup.php
-sudo php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer
+
 
 sudo chmod 766 /usr/local/bin/a2hoster  >> /dev/null
 echo "Installing complete. Check functionality by running shell with sudo a2hoster"
